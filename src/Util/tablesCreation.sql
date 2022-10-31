4c7d071eb778cd34bd608190579404d88fab322a
AdeWYyse <adelinewyse@gmail.com>
2022-10-31 16:15:57 -0300

    Database tables sql

---
+++ src/Util/tablesCreation.sql
@@ -0,0 +1,26 @@
+CREATE TABLE filme (
+    id SERIAL PRIMARY KEY ,
+    titulo char(50) ,
+    genero char(50) ,
+    ranking int,
+    foiAssistido boolean
+);
+
+CREATE TABLE serie (
+   id SERIAL PRIMARY KEY ,
+   titulo char(50) ,
+   genero char(50) ,
+   ranking int,
+   numEpisodios int ,
+   numEpisodiosAssistidos int
+);
+
+
+CREATE TABLE jogo (
+   id SERIAL PRIMARY KEY ,
+   titulo char(50) ,
+   genero char(50) ,
+   ranking int,
+   numEpisodios int ,
+   numEpisodiosAssistidos int
+);
